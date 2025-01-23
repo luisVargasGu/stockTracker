f500892dd27d7c74c46f3ee6065d3a9dfdc286ea
luisVargasGu <vargasluis012@gmail.com>
2025-01-23 08:30:04 -0500

    Example import.

--- services/user-service/main.go
+++ services/user-service/main.go
@@ -1,8 +1,20 @@
 package userservice
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/luisVargasGu/stockTracker/common/middleware"
+)
 
 func main() {
+	tokenService := middleware.NewTokenService(os.Getenv("JWT_SECRET"))
+
+	r := gin.New()
+
+	// Users Auth now
+	r.Use(middleware.AuthMiddleware(tokenService))
 	fmt.Printf(
 		"started",
 	)
