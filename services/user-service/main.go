5cb5ff45308c4bb5a41631fc164e67008e44a9c4
luisVargasGu <vargasluis012@gmail.com>
2025-01-23 17:33:47 -0500

    Exemplar use of logging middle ware and couple todos for later.

--- services/user-service/main.go
+++ services/user-service/main.go
@@ -2,19 +2,31 @@ package userservice
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/luisVargasGu/stockTracker/common/middleware"
+	"go.uber.org/zap"
 )
 
 func main() {
 	tokenService := middleware.NewTokenService(os.Getenv("JWT_SECRET"))
 
+	logger, err := zap.NewDevelopment() // For production: zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	defer logger.Sync()
+
 	r := gin.New()
 
-	// Users Auth now
+	r.Use(middleware.LoggingMiddleware(logger))
+
+	// Routes after this requre auth
 	r.Use(middleware.AuthMiddleware(tokenService))
+
+	// TODO: When registering routes with handlers pass the logger
 	fmt.Printf(
 		"started",
 	)
