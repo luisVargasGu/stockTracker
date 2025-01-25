183f970a414f1ddaa22eced32d465e9cd56b143e
luisVargasGu <vargasluis012@gmail.com>
2025-01-25 18:35:26 -0500

    Renaming username to email as it makes more sense for the user Struct. Added possible TODO as UserInfo may not be used/needed.

--- services/user-service/models/user.go
+++ services/user-service/models/user.go
@@ -1,14 +1,13 @@
 package models
 
 import (
-	"context"
 	"time"
 )
 
 type User struct {
 	ID           int        `json:"id" db:"id"`
 	Name         string     `json:"name" db:"name"`
-	Username     string     `json:"email" db:"username"`
+	Email        string     `json:"email" validate:"email" db:"username"`
 	Role         string     `json:"role" db:"role"` // e.g., "admin", "user"
 	PasswordHash string     `json:"-" db:"password_hash"`
 	Avatar       []byte     `json:"avatar,omitempty" db:"avatar"`
@@ -18,41 +17,10 @@ type User struct {
 	DeletedAt    *time.Time `json:"deletedAt,omitempty" db:"deleted_at"`
 }
 
+// TODO: may not need this
 type UserInfo struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
 	Username  string `json:"username"`
 	AvatarURL string `json:"avatarUrl,omitempty"`
-}
-
-type UserStore interface {
-	GetUserByEmail(ctx context.Context, email string) (*User, error)
-	GetUserByID(ctx context.Context, id int) (*User, error)
-	GetUsers(ctx context.Context, offset, limit int) ([]*User, error)
-	CreateUser(ctx context.Context, user *User) (*User, error)
-	UpdateUser(ctx context.Context, id int, updates map[string]interface{}) (*User, error)
-	DeleteUser(ctx context.Context, id int) (bool, error)
-}
-
-type LoginUserPayload struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
-}
-
-type RegisterUserPayload struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
-	Avatar   string `json:"avatar,omitempty"` // URL or base64 encoded
-}
-
-type LoginResponse struct {
-	Success bool       `json:"success"`
-	Message string     `json:"message"`
-	User    *UserInfo  `json:"user,omitempty"`
-	Token   *AuthToken `json:"token,omitempty"` // For authentication
-}
-
-type AuthToken struct {
-	Token     string    `json:"token"`
-	ExpiresAt time.Time `json:"expiresAt"`
 }
