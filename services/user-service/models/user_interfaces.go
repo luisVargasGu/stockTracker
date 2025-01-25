46676dcc523b72c79be1a5aeb2f5a5962645239e
luisVargasGu <vargasluis012@gmail.com>
2025-01-25 18:38:18 -0500

    Broke up interface and payload definitions for better maintanability.

---
+++ services/user-service/models/user_interfaces.go
@@ -0,0 +1,22 @@
+package models
+
+import "context"
+
+type UserRepository interface {
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+	GetUserByID(ctx context.Context, id int) (*User, error)
+	GetUsers(ctx context.Context, offset, limit int) ([]*User, error)
+	CreateUser(ctx context.Context, user *User) (*User, error)
+	UpdateUser(ctx context.Context, id int, updates map[string]interface{}) (*User, error)
+	DeleteUser(ctx context.Context, id int) (bool, error)
+}
+
+type UserService interface {
+	GetUsers(ctx context.Context, offset, limit int) ([]*User, int, error)
+	LoginUser(ctx context.Context, user LoginUserPayload) ([]*User, error)
+	GetUserByID(ctx context.Context, id int) (*User, error)
+	RegisterUser(ctx context.Context, user RegisterUserPayload) (*User, error)
+	AuthenticateUser(ctx context.Context, payload LoginUserPayload) (*LoginResponse, error)
+	UpdateUser(ctx context.Context, id int, updates map[string]interface{}) (*User, error)
+	DeleteUser(ctx context.Context, id int) error
+}
---
+++ services/user-service/models/user_payloads.go
@@ -0,0 +1,37 @@
+package models
+
+import "time"
+
+type LoginUserPayload struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type RegisterUserPayload struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	Avatar   string `json:"avatar,omitempty"` // URL or base64 encoded
+}
+
+type LoginResponse struct {
+	Success bool       `json:"success"`
+	Message string     `json:"message"`
+	User    *UserInfo  `json:"user,omitempty"`
+	Token   *AuthToken `json:"token,omitempty"` // For authentication
+}
+
+type UpdateUserPayload struct {
+	Name   *string `json:"name,omitempty"`
+	Email  *string `json:"email,omitemty" validate:"email"`
+	Avatar *[]byte `json:"avatar,omitempty"`
+}
+
+type Pagination struct {
+	Offset int
+	Limit  int
+}
+
+type AuthToken struct {
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expiresAt"`
+}
